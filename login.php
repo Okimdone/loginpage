b699a621475d42c6c27ce7aa14c13b51973ed2bd
Okimdone <dante-street-fighter@outlook.fr>
2019-03-04 20:35:34 +0100

    function islogin and php.login

--- login.php
+++ login.php
@@ -5,8 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <?php require_once("php/functions.php") ?>
 </head>
 <body>
     HAHAAAAAAAAA
+    <?php islogin("admin", "pass"); ?>
 </body>
 </html>
