1906a1f5e76edb4f1486a6aa845ac9d1977a9976
Okimdone <dante-street-fighter@outlook.fr>
2019-03-04 19:59:16 +0100

    php functions s prototypes

---
+++ php/functions.php
@@ -0,0 +1,16 @@
+<?php 
+    //globale variables to store login infos
+    global $UNAME ; $UNAME = array();
+    global $PASSWDS;$PASSWDS = array();
+
+    function cryptthis ($passwd ) {
+        return $passwd;
+    }
+
+    function load_csv () {
+
+    }
+
+    function islogin ($uname, $passwd) {
+        
+    }
