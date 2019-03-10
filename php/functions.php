9db50c9b973b85af1049ca9331d8d4aa560cce19
Okimdone <dante-street-fighter@outlook.fr>
2019-03-10 01:33:22 +0100

    changes to check

--- php/functions.php
+++ php/functions.php
@@ -2,45 +2,44 @@
     //globale variables to store login infos
     global $UNAME ; $UNAME = array();
     global $PASSWDS;$PASSWDS = array();
-    global $f ;$f ='data\users.csv' ;
+    global $f ;$f ='data\users.csv' ;//change the slash Windows/Linux
     load_csv();
     
-    function cryptthis ($passwd ) {
-         $tab = array();
-         $tab = $passwd ;
-         $strlen = strlen($tab)-1;
-         for ($i=0; $i <$strlen/2 ; $i++) { 
+    function cryptthis (&$passwd ) {
+        $tab = $passwd ;
+        $strlen = strlen($tab)-1;
+        for ($i=0; $i <$strlen/2 ; $i++) { 
             $swap = $tab[$strlen] ;
             $tab[$strlen] = $tab[$i];
-             $tab[$i] = $swap ;
+            $tab[$i] = $swap ;
             $strlen--;
-         }
-         $passwd = $tab ;
-        return $passwd;
+        }
+        return $tab;
     }
-        function enregistrer($uname,$mdp){
+    
+    function enregistrer($uname,&$mdp){
         global $f;
-        $a = cryptthis($mdp);
-        $b = "\n".$uname.';'.$a ;
+        $a = cryptthis( $mdp);
+        $b = $uname.';'.$a ."\n" ;
         if (file_exists($f)) {
-        $file = fopen($f,'a+');
-        fputs($file,$b,strlen($b));
-        echo "felicitation vous etes un membre :) $b!";
-    }
-    	fclose($file);
+            $file = fopen($f,'a+');
+            fputs($file,$b,strlen($b));
+            echo "felicitation vous etes un membre :) $b!";
+            fclose($file);
+        }
     }
     function load_csv () {
 		global $UNAME ;
 	    global $PASSWDS ;
 	    global $f;
 	    if (file_exists($f)) {
-	    $file = fopen($f, 'r');
-    	while ( !feof($file)) { 	
-    	 	$line = fgets($file);
-    	 	$tab = explode(";", $line);
-    	 	$UNAME[] = $tab[0]; $PASSWDS[] = $tab[1];
-    	}
-    	fclose($file);
+            $file = fopen($f, 'r');
+            while ( !feof($file)) { 	
+                $line = fgets($file);
+                $tab = explode(";", $line);
+                $UNAME[] = $tab[0]; $PASSWDS[] = $tab[1];
+            }
+            fclose($file);
 	    }
     }
 
